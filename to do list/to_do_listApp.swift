848813513ded1e6223045c5bb0527abeab387984
seba-sanchis <seba.sanchis@gmail.com>
2023-11-17 19:09:01 -0300

    Initial Commit

---
+++ to do list/to_do_listApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_do_listApp.swift
+//  to do list
+//
+//  Created by Sebastian Sanchis on 17/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct to_do_listApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
